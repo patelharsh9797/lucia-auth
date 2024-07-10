d111af7691bcca6b972191e789017abfcf086048
Harsh Patel <patelharsh9797@gmail.com>
2024-07-10 15:26:57 +0530

    Shadcn init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { db } from "@/server/db";
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default async function Home() {
   const posts = await db.query.tests.findMany();
   return (
     <main className="flex min-h-screen flex-col items-center gap-8 p-24">
       <h1>Names: </h1>
+      <Button variant={"destructive"}>Shadcn Button</Button>
       {posts.map((post) => (
         <p key={post.id}>{post.name}</p>
       ))}
